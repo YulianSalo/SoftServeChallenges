60c72a1cf1f3a9bcc441b2fc5d30e83993284997
DmytroSavchuk <39459668+DmytroSavchuk@users.noreply.github.com>
2019-10-24 20:21:19 +0300

    Create easy_challenge2.java

---
+++ DmytorSavchuk/easy_challenge2.java
@@ -0,0 +1,15 @@
+class Main{
+  public static void main(String[] args)
+  {
+    System.out.println(Integer.parseInt(args[0]));
+  }
+  public int FirstFactorial(int num)
+  {
+    int factorial = 1;
+    for (int i = num; i < 2; i--)
+    {
+      factorial *= i;
+    }
+    return factorial;
+  }
+}
