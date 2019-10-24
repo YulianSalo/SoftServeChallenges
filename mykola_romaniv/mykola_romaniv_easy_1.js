8ff6a0fba308e06e8f7fc0ea108e8ed90ac27010
Mykola Romaniv <mykolaromaniw@gmail.com>
2019-10-24 20:23:51 +0300

    done easy1

---
+++ mykola_romaniv/mykola_romaniv_easy_1.js
@@ -0,0 +1,15 @@
+function LongestWord(sen) {
+    let wordList = sen.split(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g);
+    let maxI = 0;
+    let maxLen = 0;
+    for (let i in wordList) {
+        if(wordList[i].length > maxLen) {
+            maxLen = wordList[i].length;
+            maxI = i;
+        }
+    }
+    console.log(wordList[maxI]);
+}
+
+
+LongestWord('some, words! yyyy');
